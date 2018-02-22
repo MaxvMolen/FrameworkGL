b189b3ec75fe9033437fbf5bb2d8765de695ce98
MaxvMolen <maxharm1811@gmail.com>
2018-02-22 11:47:44 +0100

    Added deltaTime to the rotation of the rgba image

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -19,12 +19,20 @@ int main( void )
 	float rot_z = 0.0f;
 
 	do {
+		// glfwGetTime is called only once, the first time this function is called
+		static double lastTime = glfwGetTime();
+
+		// Compute time difference between current and last frame
+		double currentTime = glfwGetTime();
+		float deltaTime = float(currentTime - lastTime);
+
 		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Compute the ViewMatrix from keyboard and mouse input (see: camera.h/cpp)
 		computeMatricesFromInputs(renderer.window());
 
+		// print position of the mouse on screen
 		//glm::vec3 cursor = getCursor(); // from Camera
 		//printf("(%f,%f)\n",cursor.x, cursor.y);
 
@@ -32,12 +40,15 @@ int main( void )
 		renderer.renderSprite(pencils, 400, 300, 1.0f, 1.0f, 0.0f);
 		renderer.renderSprite(kingkong, 900, 400, 1.0f, 1.0f, 0.0f);
 		renderer.renderSprite(rgba, renderer.width()/2, renderer.height()/2, 3.0f, 3.0f, rot_z);
-		rot_z += 0.03f;
+		rot_z += 0.3f * deltaTime;// * deltaTime to have the same rotationspeed no matter what fps you have
+		// the default rotation speed of the rgba image = 0.03f
 
 		// Swap buffers
 		glfwSwapBuffers(renderer.window());
 		glfwPollEvents();
 
+		// For the next frame, the "last time" will be "now"
+		lastTime = currentTime;
 	} // Check if the ESC key was pressed or the window was closed
 	while( glfwGetKey(renderer.window(), GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
 		   glfwWindowShouldClose(renderer.window()) == 0 );
