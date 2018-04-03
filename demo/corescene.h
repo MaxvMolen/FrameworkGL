9ef4b126545f625c7f18335ab7715beb2623766c
MaxvMolen <maxharm1811@gmail.com>
2018-04-03 09:23:53 +0200

    After problems with camera returned to older code

--- demo/corescene.h
+++ demo/corescene.h
@@ -32,7 +32,7 @@ public:
 	/// @brief update this Corescene. This function is Pure virtual. User MUST implement this in subclass.
 	/// @param deltaTime The time that's passed since the last update.
 	/// @return void
-	virtual void update(float deltaTime) = 0;
+	virtual void update(float deltaTime);
 	/// @brief quit will close the program
 	/// @param deltaTime the elapsed time in seconds
 	/// @return void
