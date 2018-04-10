52858651dca1a082ae5bbec0de504b29b4dd2d42
MaxvMolen <maxharm1811@gmail.com>
2018-04-10 09:55:17 +0200

    Updated input handleMouse

--- FrameworkMvdM/input.cpp
+++ FrameworkMvdM/input.cpp
@@ -21,6 +21,11 @@ Input::Input()
 		_keysUp[i] = false;
 		_keysDown[i] = false;
 	}
+	for (unsigned int i = 0; i<GLFW_MOUSE_BUTTON_LAST; i++) {
+		_mouse[i] = false;
+		_mouseUp[i] = false;
+		_mouseDown[i] = false;
+	}
 }
 
 Input::~Input()
@@ -49,10 +54,14 @@ void Input::updateInput(GLFWwindow* window) {
 	// Fix cursor position if window size is different from the set resolution
 	_mouseX = ((float)SWIDTH / _windowWidth) * _mouseX;
 	_mouseY = ((float)SHEIGHT / _windowHeight) * _mouseY;
+	// mouse buttons
+	for (unsigned int i = 0; i<GLFW_MOUSE_BUTTON_LAST; i++) {
+		_handleMouse(i);
+	}
 }
 
 void Input::_handleMouse(unsigned int key) {
-	if (glfwGetKey(_window, key) == GLFW_PRESS) {
+	if (glfwGetMouseButton(_window, key) == GLFW_PRESS) {
 		if (_mouse[key] == false) { // if first time pressed down
 			_mouse[key] = true;
 			_mouseDown[key] = true;
@@ -65,7 +74,7 @@ void Input::_handleMouse(unsigned int key) {
 		}
 	}
 
-	if (glfwGetKey(_window, key) == GLFW_RELEASE) {
+	if (glfwGetMouseButton(_window, key) == GLFW_RELEASE) {
 		if (_mouse[key] == true) { // still pressed
 			_mouse[key] = false;
 			_mouseUp[key] = true;
